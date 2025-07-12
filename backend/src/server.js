4f0b0d5003a92034b62b19b1419d46fd60015a00
imNandini19 <n210843@rguktn.ac.in>
2025-07-12 23:16:41 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,22 @@ import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if(process.env.NODE_ENV !== "production"){
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );  
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -24,6 +29,16 @@ app.use(rateLimiter);
 //   next();
 // });
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+  })
+}
+
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
